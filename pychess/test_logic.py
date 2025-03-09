379d48b2ba0de2b67cfd223a4fea7c2cc2e00a7e
jvogel <joevogel501@gmail.com>
2025-03-08 22:45:59 -0800

    initialized unit tests, added decomposition

---
+++ pychess/test_logic.py
@@ -0,0 +1,49 @@
+import pytest
+
+def test_moving_piece():
+    pass
+
+def test_not_moving_piece(): # click on piece and click again on same piece (no moving)
+    pass
+
+def test_capture_piece():
+    pass
+
+def test_blocked_move(): # attempting to move onto tile that has same color piece already
+    pass
+
+def test_win_cases(): # result field key: empty-in progress, w-white win, b-black win, d-draw
+    pass
+
+def test_stalemate(): # draw
+    pass
+
+def test_legal_moves(): # for all pieces, reset board between each test
+    pass
+
+def test_illegal_moves(): # for all pieces, reset board between each test
+    pass
+
+def test_moving_into_checK(): # shouldn't be possible
+    pass
+
+def test_initialize_board(): # ensure all pieces present and in the right spot
+    pass
+
+def test_check_conditions():
+    pass
+
+def test_checkmate_conditions():
+    pass
+
+def test_pawn_promotion():
+    pass
+
+def test_en_passant():
+    pass
+
+def test_queenside_castling():  
+    pass
+
+def test_kingside_castling():
+    pass
