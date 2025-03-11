61bc3b6b7d905c9345b71cab918d601181c76a27
jvogel <joevogel501@gmail.com>
2025-03-10 17:06:51 -0700

    pseudocode and things

--- pychess/logic/chess_logic.py
+++ pychess/logic/chess_logic.py
@@ -36,7 +36,7 @@ class ChessLogic:
         Function to make a move if it is a valid move. This function is called everytime a move in made on the board
 
         Args:
-            move (str): The move which is proposed. The format is the following: starting_sqaure}{ending_square}
+            move (str): The move which is proposed. The format is the following: starting_square}{ending_square}
             
             i.e. e2e4 - This means that whatever piece is on E2 is moved to E4
 
@@ -45,4 +45,89 @@ class ChessLogic:
         """
         
         #Implement this
+        # parse starting tile and destination tile into two variables
+        # figure out if there is a piece on starting tile, if not, invalid move
+        #   if piece is not current user's piece, invalid move (keep track of turns)
+        # append piece letter to result string (no letter for pawn)
+        # call respective function for that piece
+        # if valid, check if destination already has a piece
+        #   if opponent piece, capture (append x to denote capture in extended chess notation)
+        #   if own piece, invalid move 
+        # check if moving put user into check, if it did, invalid move
+        # 
+        pass
+
+        '''
+        pseudocode:
+            str start_tile = move. first two letters
+            str target_tile = move. # last two letters
+            
+            # get piece on start_tile
+            piece = self.board[start_tile]
+            # convert piece to lowercase so we can handle both white and black pieces
+            piece = tolower(piece)
+
+            switch(piece)
+                case 'p': # pawn
+                    if !move_pawn(self, start_tile, target_tile)
+                        return '' # invalid move
+                
+                case 'n': # knight
+                    if !move_knight(self, start_tile, target_tile)
+                        return ''
+
+                case 'b': # bishop
+                    if !move_bishop(self, start_tile, target_tile)
+                        return ''
+                
+                case 'r': # rook
+                    if !move_rook(self, start_tile, target_tile)
+                        return ''
+                
+                case 'q': # queen
+                    if !move_queen(self, start_tile, target_tile)
+                        return ''
+                
+                case 'k': # king
+                    if !move_king(self, start_tile, target_tile)
+                        return ''
+
+                default:
+                    return ''
+
+            if moved_to_check()
+                return ''   # player moved into check
+        '''
+
+        
+    # each move function returns true if the move is possible (only considering tiles)
+    # do not consider external factors like other pieces 
+    # some functions return tuples of bools, for special cases like en passant, promotion, castling, etc.
+    def move_pawn(self, start_tile: str, target_tile: str) -> tuple(bool, bool, bool, bool):
+        pass
+        # handle capturing diagonally
+        # handle en passant
+        # handle promotion
+
+    def move_knight(self, start_tile: str, target_tile: str) -> bool:
+        pass
+
+    def move_bishop(self, start_tile: str, target_tile: str) -> bool:
+        pass
+
+    def move_rook(self, start_tile: str, target_tile: str) -> bool, bool:
+        pass
+        # call castling if inputs match castling tiles
+        #   handle going through check while castling (king can not move into check while castling)
+
+    def move_queen(self, start_tile: str, target_tile: str) -> bool:
+        pass
+
+    def move_king(self, start_tile: str, target_tile: str) -> bool:
+        pass
+
+    def move_castling(self, start_tile: str, target_tile: str) -> bool:
+        pass
+
+    def moved_to_check():
         pass
